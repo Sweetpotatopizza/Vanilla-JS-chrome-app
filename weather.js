5988884b7bd290dc755973e10d2e71dd60504c88
Joohyun Lim <39722436+Sweetpotatopizza@users.noreply.github.com>
2020-09-06 23:28:13 +0900

    Background
    Background

---
+++ weather.js
@@ -0,0 +1,38 @@
+const API_KEY = "37c5aff909654867a2654ccf49383f67";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError(position) {
+  console.log("Cant access geo location");
+}
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    //getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
